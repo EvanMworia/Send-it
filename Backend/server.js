39629f64e2acd1b32ac2676f54c3c6b4f5080921
EvanMworia <evannji99@gmail.com>
2025-02-09 12:04:08 +0300

    Refactor: Folder Structure and Modified server,js

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import userRouter from './routes/userRoutes.js';
+import parcelRouter from './routes/parcelRoutes.js';
+
+const app = express();
+
+// Middleware
+app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+app.use(express.json()); // Parse JSON requests
+
+// Routes
+app.use('/users', userRouter);
+app.use('/parcels', parcelRouter);
+
+// Start Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
