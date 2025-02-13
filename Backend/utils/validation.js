8ab98af864153901ae3c528de899df8791477c2a
EvanMworia <evannji99@gmail.com>
2025-02-13 14:56:01 +0300

    Feature: Added JOI for registerUserSchema

--- Backend/utils/validation.js
+++ Backend/utils/validation.js
@@ -4,7 +4,7 @@ export const registerUserSchema = Joi.object({
 	FullName: Joi.string().required(),
 	Email: Joi.string().email().required(),
 	// PasswordHash: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$')),
-	PasswordHash: Joi.string().required(),
+	Password: Joi.string().required(),
 	ProfilePicture: Joi.string().required(),
 	Phone: Joi.string().required(),
 	Role: Joi.string().required(),
