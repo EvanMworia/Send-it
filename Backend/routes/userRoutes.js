3774197685e0c5559e151049e962aef9789e0672
EvanMworia <evannji99@gmail.com>
2025-02-13 15:11:17 +0300

    Refactor: Modified Conflicting routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,10 +1,21 @@
 import express from 'express';
-import { getUsers, DeleteUser, registerNewUser, GetUserById } from '../controllers/userController.js';
+import {
+	getUsers,
+	DeleteUser,
+	registerNewUser,
+	GetUserById,
+	GetUserByEmail,
+	login,
+} from '../controllers/userController.js';
+import { authenticateJWT } from '../middleware/authMiddlewares.js';
 const userRouter = express.Router();
 
 userRouter.post('/registerUser', registerNewUser);
-userRouter.get('/getAllUsers', getUsers);
-userRouter.get('/findUser/:id', GetUserById);
+userRouter.post('/login', login);
+userRouter.get('/getAllUsers', authenticateJWT, getUsers);
+userRouter.get('/getUserById/:id', GetUserById);
+userRouter.get('/getUserByEmail/:email', GetUserByEmail);
+
 userRouter.delete('/deleteUser/:id', DeleteUser);
 
 export default userRouter;
